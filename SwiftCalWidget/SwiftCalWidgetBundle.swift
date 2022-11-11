a7708fed68320c0e263baa82349ca0d73565aeb3
fabianskier <fabianskier@icloud.com>
2022-11-11 00:47:54 -0300

    CO-0005: Add App Group & Core Data Migration
    Signed-off-by: fabianskier <fabianskier@icloud.com>

---
+++ SwiftCalWidget/SwiftCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCalWidgetBundle.swift
+//  SwiftCalWidget
+//
+//  Created by Oscar Cristaldo on 2022-11-11.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftCalWidget()
+    }
+}

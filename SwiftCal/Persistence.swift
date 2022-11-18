4a2edb2c21791566e35b3fc0f84e325aaf9ea665
fabianskier <fabianskier@icloud.com>
2022-11-17 22:46:44 -0300

    CO-0007: Fix persistence
    Signed-off-by: fabianskier <fabianskier@icloud.com>

--- SwiftCal/Persistence.swift
+++ SwiftCal/Persistence.swift
@@ -9,13 +9,24 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
+    let databaseName = "SwiftCal.sqlite"
+    
+    var oldStoreURL: URL {
+        let directory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        return directory.appending(path: databaseName)
+    }
+    
+    var sharedStoreURL: URL {
+        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.py.com.fabianskier.SwiftCal")!
+        return container.appending(path: databaseName)
+    }
+    
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-
+        
         let startDate = Calendar.current.dateInterval(of: .month, for: .now)!.start
-
+        
         for dayOffset in 0..<30 {
             let newDay      = Day(context: viewContext)
             newDay.date     = Calendar.current.date(byAdding: .day, value: dayOffset, to: startDate)
@@ -24,37 +35,47 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
         return result
     }()
-
+    
     let container: NSPersistentContainer
-
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "SwiftCal")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else if !FileManager.default.fileExists(atPath: oldStoreURL.path) {
+            container.persistentStoreDescriptions.first!.url = sharedStoreURL
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        migrateStore(for: container)
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func migrateStore(for container: NSPersistentContainer) {
+        let coordinator = container.persistentStoreCoordinator
+        
+        guard let oldStore = coordinator.persistentStore(for: oldStoreURL) else { return }
+        
+        do {
+            let _ = try coordinator.migratePersistentStore(oldStore, to: sharedStoreURL, type: .sqlite)
+            print("🏁 Migration successful")
+        } catch {
+            fatalError("Unable to migrate to shared store")
+        }
+        
+        do {
+            try FileManager.default.removeItem(at: oldStoreURL)
+            print("🗑️ Old store deleted")
+        } catch {
+            print("Unable to delete oldStore")
+        }
     }
 }
---
+++ SwiftCalWidget/SwiftCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCalWidgetBundle.swift
+//  SwiftCalWidget
+//
+//  Created by Oscar Cristaldo on 2022-11-17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftCalWidget()
+    }
+}
